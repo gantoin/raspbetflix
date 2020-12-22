9326b3ab5b846e762f9528430e92cd55988fee6b
Antoine <m.gauthier.antoine@gmail.com>
2020-12-22 20:01:09 +0100

    Fix rotate logo & typo

--- front/src/components/Info.js
+++ front/src/components/Info.js
@@ -17,7 +17,7 @@ class Info extends React.Component {
             <p>Un fois cela fait, allez dans votre bibliothèque et réclamez-la. Vous pourrez ensuite accéder à cette bibliothèque sur un client Plex (TV, smartphone, ...) en vous connectant au même compte Google et en étant bien sûr connecté à votre réseau (wifi, ethernet, etc.)</p>
             <p>Passons maintenant à la configuration de <a href={"http://" + window.location.hostname + ":9092/"}>Deluge</a>, le client Web pour télécharger des torrents.</p>
             <p>Le mot de passe par défaut est 'deluge' ; je vous recommande de le changer. Choisissez votre hôte local, qui devrait être déjà présent : <i>127.0.0.1</i> ou <i>localhost</i> et cliquez sur 'Connect'.</p>
-            <p>Vous pouvez dès maintenant télécharger un fichier via n'importe quel torrent. N'oubliez pas de chnager la localication de destination pour le fichier. Prenez le dossier précédemment crée, par exemple <i>/media/usb0/films</i> s'il s'agit d'un film.</p>
+            <p>Vous pouvez dès maintenant télécharger un fichier via n'importe quel torrent. N'oubliez pas de changer la localication de destination pour le fichier. Prenez le dossier précédemment créé, par exemple <i>/media/usb0/films</i> s'il s'agit d'un film.</p>
             <p>La configuration VPN est en cours de développement.</p>
         </div>;
     }
